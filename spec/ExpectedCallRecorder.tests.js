abc8c2dd744186bf725575818173204e8762931e
fschwiet <franks@.allrecipes.corp>
2010-09-13 17:22:59 -0700

    adding ExpectedCallRecorder

---
+++ spec/ExpectedCallRecorder.tests.js
@@ -0,0 +1,92 @@
+﻿
+
+describe("ExpectedCallRecorder", function() {
+
+    var expectedReturnValue1;
+    var expectedReturnValue2;
+    var expectedReturnValue3;
+
+    var sut;
+
+    var foo;
+
+    beforeEach(function() {
+        expectedReturnValue1 = {a:1};
+        expectedReturnValue2 = {b:2};
+        expectedReturnValue3 = {c:3};
+
+        sut = new ExpectedCallRecorder();
+
+        sut.record("method1", [1, 2, 3], expectedReturnValue1);
+        sut.record("method2", [4, 5, 6], expectedReturnValue2);
+        sut.record("method1", [7, 8, 9], expectedReturnValue3);
+
+        foo = {
+            m1: function() { },
+            m2: function() { }
+        };
+
+        spyOn(foo, "m1").andCallFake(sut.recordedMethod("method1"));
+        spyOn(foo, "m2").andCallFake(sut.recordedMethod("method2"));
+    });
+
+    it("allows for record/playback", function() {
+
+        expect(foo.m1(1, 2, 3)).toEqual(expectedReturnValue1);
+        expect(foo.m2(4, 5, 6)).toEqual(expectedReturnValue2);
+        expect(foo.m1(7, 8, 9)).toEqual(expectedReturnValue3);
+    });
+
+    describe("and detects incorrect playback", function() {
+
+        beforeEach(function() {
+            expect(foo.m1(1, 2, 3)).toEqual(expectedReturnValue1);
+        });
+
+        it("of wrong method", function() {
+
+            expect(function() {
+            
+                foo.m1(4,5,6);        
+            }).toThrow("Call recording #2 failed: Expected call to method 'method2', actual method called was 'method1'.");
+        });
+        
+        it("with wrong parameters", function() {
+        
+            expect(function() {
+            
+                foo.m2(4,1,6);        
+            }).toThrow("Call recording #2 failed: Mismatch at parameter #2, expected '5' but was '1'.");
+        });
+        
+        it("with too many parameters", function() {
+            expect(function() {
+            
+                foo.m2(4,5,6,7);        
+            }).toThrow("Call recording #2 failed: Expected 3 parameters, received 4 parameters.");
+        });
+        
+        it("with too few parameters", function() {
+            expect(function() {
+            
+                foo.m2(4,5);        
+            }).toThrow();
+        });
+        
+        it("without all calls matched,", function() {
+        
+            expect(function() {
+                sut.verify();
+            }).toThrow("Call record and playback failed, received 1 call when expecting 3.");
+        });
+        
+        it("of extra method", function() {
+            expect(foo.m2(4, 5, 6)).toEqual(expectedReturnValue2);
+            expect(foo.m1(7, 8, 9)).toEqual(expectedReturnValue3);
+            
+            expect(function() {
+                expect(foo.m1()).toEqual(expectedReturnValue3);
+            }).toThrow("Call record and playback failed, unexpected call to 'method1'.");
+        });        
+    });
+});
---
+++ src/ExpectedCallRecorder.js
@@ -0,0 +1,59 @@
+﻿
+
+function ExpectedCallRecorder() {
+    this._recordedCalls = [];
+    this._position = 0;
+}
+
+ExpectedCallRecorder.prototype = {
+    record: function(name, parameters, returnValue) {
+        this._recordedCalls.push({
+            name: name,
+            parameters: parameters,
+            returnValue: returnValue
+        });
+    },
+    recordedMethod: function(name) {
+
+        var that = this;
+
+        return function() {
+
+            if (that._recordedCalls.length <= that._position) {
+                throw new Error("Call record and playback failed, unexpected call to '" + name + "'.");
+            }
+
+            var expectedCall = that._recordedCalls[that._position];
+
+            if (name != expectedCall.name)
+                that.throwError("Expected call to method '" + expectedCall.name + "', actual method called was '" + name + "'.");
+
+            for (var i = 0; i < expectedCall.parameters.length; i++) {
+
+                if (expectedCall.parameters[i] != arguments[i]) {
+                    that.throwError("Mismatch at parameter #" + (i + 1)
+                            + ", expected '" + jasmine.pp(expectedCall.parameters[i]) + "'"
+                            + " but was '" + jasmine.pp(arguments[i]) + "'.");
+                }
+            }
+
+            if (arguments.length > expectedCall.parameters.length) {
+                that.throwError("Expected " + expectedCall.parameters.length + " parameters, received " + arguments.length + " parameters.");
+            }
+
+            that._position++;
+
+            return expectedCall.returnValue;
+        };
+    },
+    throwError: function(description) {
+
+        throw new Error("Call recording #" + (this._position + 1) + " failed: " + description);
+    },
+    verify: function() {
+
+        if (this._position != this._recordedCalls.length) {
+            throw new Error("Call record and playback failed, received " + this._position + " call when expecting " + this._recordedCalls.length + ".");
+        }
+    }
+};
